6268c34c87874f28243eae9bc0bd0fb144a82a65
Tien Le <tienlx@gmail.com>
2013-05-06 20:47:07 +0700

    Remove brackets which are generated automatically by cucumber

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,17 +1,17 @@
-Given(/^there is a git repo$/) do
+Given /^there is a git repo$/ do
   create_dir 'test_repo'
   cd 'test_repo'
   run_simple('git init .')
 end
 
-Given(/^it contains a pivotal tracker token for a project$/) do
+Given /^it contains a pivotal tracker token for a project$/ do
   @api_token = @project_id = 123
   run_simple("git config --local pivotal.api-token #{@api_token}")
   run_simple("git config --local pivotal.project-id #{@project_id}")
   run_simple("git config --local pivotal.use-ssl 1")
 end
 
-Given(/^the project contains (\d+) items in the backlog$/) do |count|
+Given /^the project contains (\d+) items in the backlog$/ do |count|
   PivotalTracker::Client.clear_connections
 
   api_url = PivotalTracker::Client.api_ssl_url
@@ -25,14 +25,14 @@ Given(/^the project contains (\d+) items in the backlog$/) do |count|
               .to_return(body: File.read("#{fixtures_path}/stories.xml"))
 end
 
-When(/^I navigate to this repo$/) do
+When /^I navigate to this repo$/ do
 end
 
-When(/^I run the command to list the next items$/) do
+When /^I run the command to list the next items$/ do
   run_simple('pt ls')
 end
 
-Then(/^the output should contain (\d+) items$/) do |count|
+Then /^the output should contain (\d+) items$/ do |count|
   # TODO: Need to figure out why the `puts` output is not in all_output
 
   (1..count.to_i).each do |index|
@@ -40,12 +40,12 @@ Then(/^the output should contain (\d+) items$/) do |count|
   end
 end
 
-Then(/^it should display the task ID, description and type$/) do
+Then /^it should display the task ID, description and type$/ do
   assert_partial_output("4459994", all_output)
   assert_partial_output("Story 1", all_output)
   assert_partial_output("feature", all_output)
 end
 
-Then(/^the (\d+)th item should not be present in the output$/) do |index|
+Then /^the (\d+)th item should not be present in the output$/ do |index|
   unescape(all_output).should_not =~ /Story #{index}/
 end
